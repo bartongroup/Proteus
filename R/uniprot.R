972fce3549c37448b0e084a0c6b039580b935ec4
Marek Gierlinski <M.Gierlinski@dundee.ac.uk>
2018-03-07 14:37:38 +0000

    Improving the code

--- R/uniprot.R
+++ R/uniprot.R
@@ -27,13 +27,14 @@ tryQuery <- function(url, maxtry=5) {
 #' @details
 #'
 #' For a given list of UniProt identifiers this function will bring back
-#' annotation from UniProt servers. What information is downloaded is controlled
-#' by the \code{columns} parameter. By default it fetches gene names and protein
-#' name/description. The full list of available columns is in a vector
-#' \code{allowedUniProtColumns}.
+#' annotations from UniProt servers. What information is downloaded is
+#' controlled by the \code{columns} parameter. By default it fetches gene names
+#' and protein name/description. The full list of available columns is in a
+#' vector \code{allowedUniProtColumns}.
 #'
 #' The column names in the returned data frame are the same as in \code{columns}
-#' unless alterntive names are provided in parameter \code{col.names}.
+#' parameter, unless alternative names are provided in parameter
+#' \code{col.names}. The \code{id} column is added by default.
 #'
 #' @param unis A character vector with UniProt identifiers
 #' @param columns Data columns requested
@@ -58,10 +59,12 @@ tryQuery <- function(url, maxtry=5) {
 #' anno <- fetchFromUniProt(unis[1:100])
 fetchFromUniProt <- function(unis, columns=c("genes", "protein names"),
                              col.names=NULL, batchsize=400, verbose=FALSE) {
-  good <- columns %in% allowedUniProtColumns
-  if(sum(!good) > 0) {
-    bad <- paste(columns[!good], collapse=",")
-    stop(paste("Columns", bad, "are not allowed in UniProt queries. Here is a list of allowed columns:", paste(allowedUniProtColumns, collapse=", "), "."))
+
+  good.cols <- columns %in% allowedUniProtColumns
+  if(sum(!good.cols) > 0) {
+    bad.cols <- paste(columns[!good.cols], collapse=",")
+    stop(paste("Columns", bad.cols, "are not allowed in UniProt queries. Here is a list of allowed columns:",
+               paste(allowedUniProtColumns, collapse=", "), "."))
   }
 
   if(is.null(col.names)) {
@@ -69,20 +72,33 @@ fetchFromUniProt <- function(unis, columns=c("genes", "protein names"),
   } else {
     if(length(col.names) != length(columns)) stop("col.names has to be the same length as columns.")
   }
+
   columns <- c("id", columns)
   col.names <- c("id", col.names)
+  colstr <- paste(columns, collapse=",")
 
-  unis.good <- as.character(na.omit(unis))
-  cols <- paste(columns, collapse=",")
+  unis <- as.character(na.omit(unis))
+  # test if IDs conform to UniProt accession format
+  # regular expression from https://www.uniprot.org/help/accession_numbers
+  acc.test <- grepl("^[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}$", unis, perl=TRUE)
+  unis <- unis[acc.test]
+  if(sum(!acc.test) > 0) {
+    warning("Some identifiers do not conform to UniProt accession number format. Skipping.")
+  }
+  if(length(unis) == 0) {
+    stop("No valid UniProt accession numbers found.")
+  }
 
   url <- "http://www.uniprot.org/uniprot/"
-  batches <- split(unis.good, ceiling(seq_along(unis.good) / batchsize))
+
+  # split id list into batches of maximum size of batchsize
+  batches <- split(unis, ceiling(seq_along(unis) / batchsize))
   nbatch <- length(batches)
   res <- lapply(seq_along(batches), function(i) {
     if(verbose) cat(paste("Batch", i, "out of", nbatch, "\n"))
     ids <- batches[[i]]
     qry <- paste(paste0("id:", ids), collapse="+or+")
-    qurl <- URLencode(paste0(url, "?query=", qry, "&format=tab&columns=", cols))
+    qurl <- URLencode(paste0(url, "?query=", qry, "&format=tab&columns=", colstr))
     tryQuery(qurl)
   })
   df <- do.call(rbind, res)

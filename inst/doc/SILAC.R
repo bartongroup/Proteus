78c352a3438d59bb917a98ac799a6eccdde18120
Marek Gierlinski <M.Gierlinski@dundee.ac.uk>
2018-06-14 10:48:42 +0100

    Tweaking vignettes

--- inst/doc/SILAC.R
+++ inst/doc/SILAC.R
@@ -1,27 +1,26 @@
-## ----setup, include=FALSE-----------------------------------------------------
+## ----setup, include=FALSE------------------------------------------------
 library(proteus)
 library(ggplot2)
 library(knitr)
-options(width = 80)
 knitr::opts_chunk$set(echo = TRUE)
 
-## ---- eval=FALSE--------------------------------------------------------------
-#  vignette("unlabelled", package="proteus")
+## ---- eval=FALSE---------------------------------------------------------
+#  vignette("proteus")
 
-## ----load_data, echo=FALSE----------------------------------------------------
+## ----load_data, echo=FALSE-----------------------------------------------
 library(proteusSILAC, warn.conflicts=FALSE)
 data(proteusSILAC)
 
-## ----load_data_dummy, eval=FALSE----------------------------------------------
+## ----load_data_dummy, eval=FALSE-----------------------------------------
 #  library(proteusSILAC)
 #  data(proteusSILAC)
 
-## ----measure_columns, cache=FALSE---------------------------------------------
+## ----measure_columns, cache=FALSE----------------------------------------
 measCols <- list(
   ML = "ratio_ml"
 )
 
-## ----evidence_columns, cache=FALSE--------------------------------------------
+## ----evidence_columns, cache=FALSE---------------------------------------
 eviCols <- list(
   sequence = 'pep_sequence',
   modified_sequence = 'modified_sequence',
@@ -34,54 +33,54 @@ eviCols <- list(
   contaminant = 'potential_contaminant'
 )
 
-## ----read_evidence, eval=FALSE------------------------------------------------
+## ----read_evidence, eval=FALSE-------------------------------------------
 #  evi <- readEvidenceFile(evidenceFile, measure.cols=measCols, data.cols=eviCols, zeroes.are.missing=FALSE)
 
-## ----head_evidence------------------------------------------------------------
+## ----head_evidence-------------------------------------------------------
 head(evi)
 
-## ----metadata, eval=FALSE-----------------------------------------------------
+## ----metadata, eval=FALSE------------------------------------------------
 #  metadataFile <- system.file("extdata", "metadata.txt", package="proteusSILAC")
 #  meta <- read.delim(metadataFile, header=TRUE, sep="\t")
 
-## ----show_metadata------------------------------------------------------------
+## ----show_metadata-------------------------------------------------------
 meta
 
-## ----make_peptides, eval=FALSE------------------------------------------------
+## ----make_peptides, eval=FALSE-------------------------------------------
 #  pepdat <- makePeptideTable(evi, meta, measure.cols=measCols, aggregate.fun=aggregateMedian, experiment.type="SILAC")
 
-## ----summary_peptides---------------------------------------------------------
+## ----summary_peptides----------------------------------------------------
 summary(pepdat)
 
-## ----plot_peptide_count, fig.width=5, fig.height=4----------------------------
+## ----plot_peptide_count, fig.width=5, fig.height=4-----------------------
 plotCount(pepdat)
 
-## ----make_proteins, eval=FALSE------------------------------------------------
+## ----make_proteins, eval=FALSE-------------------------------------------
 #  prodat <- makeProteinTable(pepdat, aggregate.fun=aggregateMedian)
 
-## ----summary_proteins---------------------------------------------------------
+## ----summary_proteins----------------------------------------------------
 summary(prodat)
 
-## ----normalize_proteins-------------------------------------------------------
+## ----normalize_proteins--------------------------------------------------
 prodat.norm <- normalizeData(prodat)
 
-## ---- sample_dist, fig.width=4, fig.height=3----------------------------------
+## ---- sample_dist, fig.width=4, fig.height=3-----------------------------
 plotSampleDistributions(prodat, fill="replicate") + labs(title="Before")
 plotSampleDistributions(prodat.norm, fill="replicate") + labs(title="After")
 
-## ----limma_one_sample---------------------------------------------------------
+## ----limma_one_sample----------------------------------------------------
 res <- limmaRatioDE(prodat.norm, condition="T48")
 res <- res[order(res$P.Value),]
 head(res)
 
-## ----volcano_plot, fig.width=4, fig.height=3----------------------------------
+## ----volcano_plot, fig.width=4, fig.height=3-----------------------------
 plotVolcano(res, binhex=FALSE)
 
-## ----limma_two_samples--------------------------------------------------------
+## ----limma_two_samples---------------------------------------------------
 res2 <- limmaDE(prodat.norm)
 res2 <- res2[order(res2$P.Value),]
 head(res2)
 
-## ----protein_example, fig.width=4, fig.height=3-------------------------------
+## ----protein_example, fig.width=4, fig.height=3--------------------------
 plotIntensities(prodat.norm, id="P03372-3")
 
--- inst/doc/proteus.R
+++ inst/doc/proteus.R
@@ -1,19 +1,18 @@
-## ----setup, include=FALSE-----------------------------------------------------
+## ----setup, include=FALSE------------------------------------------------
 library(proteus)
 library(knitr)
 library(ggplot2)
-options(width = 80)
 knitr::opts_chunk$set(echo = TRUE)
 
-## ----load_data, echo=FALSE----------------------------------------------------
+## ----load_data, echo=FALSE-----------------------------------------------
 library(proteusLabelFree, warn.conflicts=FALSE)
 data(proteusLabelFree)
 
-## ----load_data_dummy, eval=FALSE----------------------------------------------
+## ----load_data_dummy, eval=FALSE-----------------------------------------
 #  library(proteusLabelFree)
 #  data(proteusLabelFree)
 
-## ----quick_start, eval=FALSE--------------------------------------------------
+## ----quick_start, eval=FALSE---------------------------------------------
 #  evidenceFile <- system.file("extdata", "evidence.txt.gz", package="proteusLabelFree")
 #  metadataFile <- system.file("extdata", "metadata.txt", package="proteusLabelFree")
 #  
@@ -25,108 +24,108 @@ data(proteusLabelFree)
 #  res <- limmaDE(prodat.med)
 #  plotVolcano_live(prodat.med, res)
 
-## ----read_evidence, eval=FALSE------------------------------------------------
+## ----read_evidence, eval=FALSE-------------------------------------------
 #  evidenceFile <- system.file("extdata", "evidence.txt.gz", package="proteusLabelFree")
 #  evi <- readEvidenceFile(evidenceFile)
 
-## ----head_evidence------------------------------------------------------------
+## ----head_evidence-------------------------------------------------------
 head(evi)
 
-## ----size_evidence------------------------------------------------------------
+## ----size_evidence-------------------------------------------------------
 dim(evi)
 format(object.size(evi), units="Mb")
 
-## ----str_measure_columns------------------------------------------------------
+## ----str_measure_columns-------------------------------------------------
 str(measureColumns)
 
-## ----str_evidence_columns-----------------------------------------------------
+## ----str_evidence_columns------------------------------------------------
 str(evidenceColumns)
 
-## ----my_columns, eval=FALSE---------------------------------------------------
+## ----my_columns, eval=FALSE----------------------------------------------
 #  myColumns <- c(evidenceColumns, mz="m/z")
 
-## ----rename_column, eval=FALSE------------------------------------------------
+## ----rename_column, eval=FALSE-------------------------------------------
 #  myColumns$protein <- "Leading Razor Protein"
 
-## ----my_columns_evidence, eval=FALSE------------------------------------------
+## ----my_columns_evidence, eval=FALSE-------------------------------------
 #  evi_mz <- readEvidenceFile(evidenceFile, data.cols=myColumns)
 
-## ----evidence_column_names----------------------------------------------------
+## ----evidence_column_names-----------------------------------------------
 evidenceFile <- system.file("extdata", "evidence.txt.gz", package="proteusLabelFree")
 evidence.columns <- readColumnNames(evidenceFile)
 evidence.columns
 
-## ----metadata-----------------------------------------------------------------
+## ----metadata------------------------------------------------------------
 metadataFile <- system.file("extdata", "metadata.txt", package="proteusLabelFree")
 meta <- read.delim(metadataFile, header=TRUE, sep="\t")
 
-## ----show_metadata------------------------------------------------------------
+## ----show_metadata-------------------------------------------------------
 meta
 
-## ----metadata_samples---------------------------------------------------------
+## ----metadata_samples----------------------------------------------------
 unique(evi$experiment)
 
-## ----make_peptides, eval=FALSE------------------------------------------------
+## ----make_peptides, eval=FALSE-------------------------------------------
 #  pepdat <- makePeptideTable(evi, meta)
 
-## ----show_peptides------------------------------------------------------------
+## ----show_peptides-------------------------------------------------------
 pepdat$tab[1:5, 1:5]
 
-## ----summary_peptides---------------------------------------------------------
+## ----summary_peptides----------------------------------------------------
 summary(pepdat)
 
-## ----plot_peptide_count, fig.width=5, fig.height=4----------------------------
+## ----plot_peptide_count, fig.width=5, fig.height=4-----------------------
 plotCount(pepdat)
 
-## ----plot_detection_similarity, fig.width=5, fig.height=4---------------------
+## ----plot_detection_similarity, fig.width=5, fig.height=4----------------
 plotDetectionSimilarity(pepdat, bin.size = 0.02)
 
-## ----plot_correlation_matrix, fig.width=6, fig.height=5-----------------------
+## ----plot_correlation_matrix, fig.width=6, fig.height=5------------------
 plotDistanceMatrix(pepdat)
 
-## ----plot_clusterin, fig.width=6, fig.height=5--------------------------------
+## ----plot_clusterin, fig.width=6, fig.height=5---------------------------
 plotClustering(pepdat)
 
-## ----remove_bad_replicate-----------------------------------------------------
+## ----remove_bad_replicate------------------------------------------------
 meta.clean <- meta[which(meta$sample  != 'BMO-7'),]
 
-## ----make_clean_peptides, eval=FALSE------------------------------------------
+## ----make_clean_peptides, eval=FALSE-------------------------------------
 #  pepdat.clean <- makePeptideTable(evi, meta.clean)
 
-## ----clean_peptides_samples---------------------------------------------------
+## ----clean_peptides_samples----------------------------------------------
 as.character(meta.clean$sample)
 
-## ---- fig.width=6, fig.height=5-----------------------------------------------
+## ---- fig.width=6, fig.height=5------------------------------------------
 plotClustering(pepdat.clean)
 
-## ----make_proteins, eval=FALSE------------------------------------------------
+## ----make_proteins, eval=FALSE-------------------------------------------
 #  prodat <- makeProteinTable(pepdat.clean)
 
-## ----summary_proteins---------------------------------------------------------
+## ----summary_proteins----------------------------------------------------
 summary(prodat)
 
-## ----normalize_proteins-------------------------------------------------------
+## ----normalize_proteins--------------------------------------------------
 prodat.med <- normalizeData(prodat)
 
-## ----normalize_proteins_quantiles---------------------------------------------
+## ----normalize_proteins_quantiles----------------------------------------
 prodat.quant <- normalizeData(prodat, norm.fun=limma::normalizeQuantiles)
 
-## ----intensity_distributions_1, fig.width=5, fig.height=4---------------------
+## ----intensity_distributions_1, fig.width=5, fig.height=4----------------
 plotSampleDistributions(prodat, title="Not normalized", fill="condition", method="violin")
 
-## ----intensity_distributions_2, fig.width=5, fig.height=4---------------------
+## ----intensity_distributions_2, fig.width=5, fig.height=4----------------
 plotSampleDistributions(prodat.med, title="Median normalization", fill="condition", method="violin")
 
-## ----intensity_distributions_3, fig.width=5, fig.height=4---------------------
+## ----intensity_distributions_3, fig.width=5, fig.height=4----------------
 plotSampleDistributions(prodat.quant, title="Quantile normalization", fill="condition", method="violin")
 
-## ----plot_mv, fig.width=6, fig.height=4, warning=FALSE------------------------
+## ----plot_mv, fig.width=6, fig.height=4, warning=FALSE-------------------
 plotMV(prodat.med, with.loess=TRUE)
 
-## ----plot_clustering_proteins, fig.width=6, fig.height=5----------------------
+## ----plot_clustering_proteins, fig.width=6, fig.height=5-----------------
 plotClustering(prodat.med)
 
-## ----protein_annotations------------------------------------------------------
+## ----protein_annotations-------------------------------------------------
 luni <- lapply(as.character(prodat$proteins), function(prot) {
  if(grepl("sp\\|", prot)) {
    uniprot <- unlist(strsplit(prot, "|", fixed=TRUE))[2]
@@ -136,91 +135,91 @@ luni <- lapply(as.character(prodat$proteins), function(prot) {
 ids <- as.data.frame(do.call(rbind, luni))
 names(ids) <- c("protein", "uniprot")
 
-## ----ids_head-----------------------------------------------------------------
+## ----ids_head------------------------------------------------------------
 head(ids)
 
-## ----fetch_from_uniprot, eval=FALSE-------------------------------------------
+## ----fetch_from_uniprot, eval=FALSE--------------------------------------
 #  annotations <- fetchFromUniProt(ids$uniprot, verbose=TRUE)
 
-## ----annotations_head---------------------------------------------------------
+## ----annotations_head----------------------------------------------------
 head(annotations)
 
-## ----merge_annotations--------------------------------------------------------
+## ----merge_annotations---------------------------------------------------
 annotations.id <- merge(ids, annotations, by.x="uniprot", by.y="id")
 annotations.id <- unique(annotations.id)
 
-## ----annotate_proteins--------------------------------------------------------
+## ----annotate_proteins---------------------------------------------------
 prodat.med <- annotateProteins(prodat.med, annotations.id)
 
-## ----evi_column_names---------------------------------------------------------
+## ----evi_column_names----------------------------------------------------
 names(evi)
 
-## ----peptide_table_modified_sequence, eval=FALSE------------------------------
+## ----peptide_table_modified_sequence, eval=FALSE-------------------------
 #  pepdat.mod <- makePeptideTable(evi, meta, sequence.col = "modified_sequence")
 #  prodat.mod <- makeProteinTable(pepdat.mod)
 
-## ----protein_groups, eval=FALSE-----------------------------------------------
+## ----protein_groups, eval=FALSE------------------------------------------
 #  pepdat.group <- makePeptideTable(evi, meta, protein.col="protein_group")
 #  prodat.group <- makeProteinTable(pepdat.group)
 
-## ----peptide_aggregate_matrix-------------------------------------------------
+## ----peptide_aggregate_matrix--------------------------------------------
 evitab.example
 
-## ----peptide_aggregate_default------------------------------------------------
+## ----peptide_aggregate_default-------------------------------------------
 aggregateSum(evitab.example)
 
-## ----peptide_aggregate_maximum_function---------------------------------------
+## ----peptide_aggregate_maximum_function----------------------------------
 aggregateMax <- function(wp) {
   s <- apply(wp, 2, function(x) max(x, na.rm=TRUE))
   return(as.vector(s))
 }
 
-## ----peptide_aggregate_maximum_example----------------------------------------
+## ----peptide_aggregate_maximum_example-----------------------------------
 aggregateMax(evitab.example)
 
-## ----peptide_aggregate_maximum_create, eval=FALSE-----------------------------
+## ----peptide_aggregate_maximum_create, eval=FALSE------------------------
 #  pepdat.max <- makePeptideTable(evi, meta, aggregate.fun=aggregateMax)
 
-## ----limma, warning=FALSE-----------------------------------------------------
+## ----limma, warning=FALSE------------------------------------------------
 res <- limmaDE(prodat.med, sig.level=0.05)
 
-## ----show_limma_res-----------------------------------------------------------
+## ----show_limma_res------------------------------------------------------
 head(res)
 
-## ----show_significant_proteins------------------------------------------------
+## ----show_significant_proteins-------------------------------------------
 res[which(res$significant), c("protein", "logFC", "adj.P.Val")]
 
-## ----limma_conditions, eval=FALSE---------------------------------------------
+## ----limma_conditions, eval=FALSE----------------------------------------
 #  res <- limmaDE(prodat.med, conditions=c("1112", "BMO"))
 
-## ----protein_detection--------------------------------------------------------
+## ----protein_detection---------------------------------------------------
 head(prodat$detect)
 
-## ----missing_condition--------------------------------------------------------
+## ----missing_condition---------------------------------------------------
 only.1112 <- which(prodat$detect$`1112` & !prodat$detect$BMO)
 only.BMO <- which(!prodat$detect$`1112` & prodat$detect$BMO)
 
-## ----missing_condition_proteins-----------------------------------------------
+## ----missing_condition_proteins------------------------------------------
 as.character(prodat$proteins[only.1112])
 
-## ----plot_fid, fig.width=4, fig.height=4, warning=FALSE-----------------------
+## ----plot_fid, fig.width=4, fig.height=4, warning=FALSE------------------
 plotFID(prodat.med)
 
-## ----plot_volcano, fig.width=4, fig.height=4, warning=FALSE-------------------
+## ----plot_volcano, fig.width=4, fig.height=4, warning=FALSE--------------
 plotVolcano(res)
 
-## ----plot_pdist, fig.width=4, fig.height=4, warning=FALSE---------------------
+## ----plot_pdist, fig.width=4, fig.height=4, warning=FALSE----------------
 plotPdist(res)
 
-## ----plot_proteins, fig.width=4, fig.height=4, warning=FALSE------------------
+## ----plot_proteins, fig.width=4, fig.height=4, warning=FALSE-------------
 plotIntensities(prodat.med, id='sp|P26263|PDC6_YEAST', log=TRUE)
 
-## ----plot_prot_peptides, fig.width=7, fig.height=6, warning=FALSE-------------
+## ----plot_prot_peptides, fig.width=7, fig.height=6, warning=FALSE--------
 plotProtPeptides(pepdat.clean, 'sp|P26263|PDC6_YEAST', prodat.med)
 
-## ----shiny_volcano, eval=FALSE------------------------------------------------
+## ----shiny_volcano, eval=FALSE-------------------------------------------
 #  plotVolcano_live(prodat.med, res)
 
-## ----shiny_fid, eval=FALSE----------------------------------------------------
+## ----shiny_fid, eval=FALSE-----------------------------------------------
 #  plotFID_live(prodat.med, res)
 
